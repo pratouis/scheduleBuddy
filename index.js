da01feacab37cd79ec2a9bf13f82efa65b9f14a8
Persis Ratouis <persis.ratouis@gmail.com>
2018-03-26 16:07:42 -0700

    following demis advice and only using rtm no webhooks

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 const { IncomingWebhook, RTMClient, WebClient } = require('@slack/client');
+// import SlackBot from 'slackbots';
+//
+// const bot = new SlackBot({
+//   token: process.env.BOT_SLACK_TOKEN,
+//   name: 'bubbabuddy'
+// });
+//
+// bot.on('start', function() {
+//   var params = {
+//        icon_emoji: ':cat:'
+//    };
+//
+//    // define channel, where bot exist. You can adjust it there https://my.slack.com/services
+//    bot.postMessageToChannel('schedulebuddy', 'meow!', params);
+//
+// });
 
 /*
 * RTM API to be used to respond to messages ?
@@ -9,30 +25,18 @@ rtm.start();
 /*
 * Web API to be used to parse through messages ?
 */
-const web = new WebClient(process.env.SLACK_TOKEN);
+// const web = new WebClient(process.env.SLACK_TOKEN);
 
 /* WEBHOOK needed to communicate with slack server and slack server to our app */
-const webhook = new IncomingWebhook(process.env.SLACK_WEBHOOK_URL);
 const currentTime = new Date().toTimeString();
 
 
-webhook.send(`The current time is ${currentTime}`, (error, resp) => {
-  if (error) {
-    return console.error(error);
-  }
-  console.log('Notification sent');
-  console.log('Waiting a few seconds for search indexes to update...');
-  setTimeout(() => {
-    console.log('Calling search.messages');
-    web.search.messages({ query: currentTime })
-      .then(resp => {
-        console.log(resp);
-        if (resp.messages.total > 0) {
-          console.log('First match:', resp.messages.matches[0]);
-        } else {
-          console.log('No matches found');
-        }
-      })
-      .catch(console.error)
-  }, 12000);
+rtm.on('message', (event) => {
+  // For structure of `event`, see https://api.slack.com/events/reaction_added
+  console.log(event);
+  rtm.addOutgoingEvent(true, 'message', { text:'hi you', channel: event.channel, reply_broadcast: true }).then((res) => {
+    // `res` contains information about the posted message
+    console.log('Message sent: ', res.ts);
+  })
+  .catch(console.error);
 });
