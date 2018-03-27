795d06165477bfe3408f5e03fd74f5b75217229b
Persis Ratouis <persis.ratouis@gmail.com>
2018-03-26 17:41:08 -0700

    adding userEmail

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const { IncomingWebhook, RTMClient, WebClient } = require('@slack/client');
-
+const { RTMClient, WebClient } = require('@slack/client');
+import axios from 'axios';
 /*
 * RTM API to be used to respond to messages ?
 */
@@ -8,7 +8,7 @@ rtm.start();
 // TODO: @Chris or @Trevor use this package to integrate with APIAI
 // const apiai = require('apiai');
 // const app = apiai(process.env.APIAI_CLIENT_TOKEN);
-
+// console.log(rtm.users);
 /*
 * Web API to be used to parse through messages ?
 */
@@ -17,20 +17,40 @@ rtm.start();
 /* WEBHOOK needed to communicate with slack server and slack server to our app */
 const currentTime = new Date().toTimeString();
 
-rtm.on('hello', (event) => {
-  console.log(event.type);
-});
 
-rtm.on('presence_change', (event) => {
-    console.log(event);
-})
 
 rtm.on('message', (event) => {
   // For structure of `event`, see https://api.slack.com/events/reaction_added
   console.log(event);
-  rtm.addOutgoingEvent(true, 'message', { text:'hi you', channel: event.channel, reply_broadcast: true }).then((res) => {
+  // console.log(event.user);
+  // TODO send @param user_msg to dialogflow to get intent/query/whatever
+  const user_msg = event.text;
+  // TODO query calendar API to see if we have access to modify calendar
+  // using email
+  const user_email = getUserEmailByID(event.user);
+  if(typeof user_email !== "string"){
+    console.log('no bueno, got error');
+    rtm.addOutgoingEvent(true, 'mess')
+  }
+  // TODO @backend let's look at associating google calendar oauth with slack acct
+
+  const response = {
+    text: 'hi you',
+    channel: event.channel,
+    reply_broadcast: true
+  }
+
+  rtm.addOutgoingEvent(true, 'message', response)
+  .then((success) => {
     // `res` contains information about the posted message
-    console.log('Message sent: ', res.ts);
+    console.log(success);
+    console.log('Message sent: ', success.ts);
   })
   .catch(console.error);
 });
+
+const getUserEmailByID = (userID) => {
+  return axios.get(`https://slack.com/api/users.info?token=${process.env.SLACK_TOKEN}&user=${userID}`)
+  .then(({data}) => data.user.profile.email)
+  .catch((err) => err);
+}
